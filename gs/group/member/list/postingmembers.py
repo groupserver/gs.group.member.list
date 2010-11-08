2c69d4751cdafcaf5c3ed9609dc351ff76b4d0bc
Michael JasonSmith <mpj17@onlinegroups.net>
2010-11-08 14:30:28 +1100

    List the posting date.

--- gs/group/member/list/postingmembers.py
+++ gs/group/member/list/postingmembers.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from urllib import urlencode
 from zope.component import createObject
+from Products.GSSearch.queries import MessageQuery
 from queries import MembersQuery
 
 class PostingMemberList(object):
@@ -11,35 +12,55 @@ class PostingMemberList(object):
         self.__parent__ = view
         self.siteInfo = createObject('groupserver.SiteInfo', context)
         self.groupInfo = createObject('groupserver.GroupInfo', context)
+        self.recentPostingMembers = []
+        self.topPostingMembers = []
 
     # TODO: Fix
     viewTopics = True
 
     def update(self):
-        self.recentPostingMembers = []
-        self.topPostingMembers = []
         if self.viewTopics:
             # If you cannot see the topics you should not see who posted
-            mq = MembersQuery(self.context.zsqlalchemy)
-            
-            ml = mq.posting_authors(self.siteInfo.id, self.groupInfo.id,
-                                    limit=6)
-            self.recentPostingMembers = [self.kulfi(uid) for uid in ml]
-            
-            tm = mq.top_posting_authors(self.siteInfo.id, 
-                                       self.groupInfo.id, limit=6)
-            for m in tm:
-                userInfo =  createObject('groupserver.UserFromId', 
-                                self.context, m['user_id'])
-                pu = PostingUser(self.context, userInfo, self.groupInfo,
-                        self.siteInfo, m['post_count'])
-                self.topPostingMembers.append(pu)
-            
-    def kulfi(self, uid):
-        return createObject('groupserver.UserFromId', self.context, uid)
+            self.update_recent_posting_members()
+            self.update_top_posting_members()
 
-class PostingUser(object):
-    def __init__(self, context, userInfo, groupInfo, siteInfo, nPosts):
+    def update_recent_posting_members(self):
+        mq = MembersQuery(self.context.zsqlalchemy)
+        ml = mq.posting_authors(self.siteInfo.id, self.groupInfo.id,
+                                limit=6)
+        for uid in ml:
+            pu = RecentPostingUser(self.context, uid, self.groupInfo, 
+                    self.siteInfo)
+            self.recentPostingMembers.append(pu)
+
+    def update_top_posting_members(self):
+        mq = MembersQuery(self.context.zsqlalchemy)
+        tm = mq.top_posting_authors(self.siteInfo.id, 
+                                   self.groupInfo.id, limit=6)
+        for m in tm:
+            pu = TopPostingUser(self.context, m['user_id'], 
+                    self.groupInfo, self.siteInfo, m['post_count'])
+            self.topPostingMembers.append(pu)
+
+class RecentPostingUser(object):
+    def __init__(self, context, uid, groupInfo, siteInfo):
+        userInfo =  createObject('groupserver.UserFromId', context, uid)
+        self.userInfo = userInfo
+        
+        mq = MessageQuery(context, context.zsqlalchemy)
+        t = createObject('groupserver.SearchTextTokens', '')
+        self.latestPost = mq.post_search_keyword(t, siteInfo.id,  
+                                                 [groupInfo.id], 
+                                                 [userInfo.id], 1)[0]
+
+        self.postUrl = '/r/post/%s' % self.latestPost['post_id']
+        
+    def __getattr__(self, name):
+        return getattr(self.userInfo, name)
+
+class TopPostingUser(object):
+    def __init__(self, context, uid, groupInfo, siteInfo, nPosts):
+        userInfo =  createObject('groupserver.UserFromId', context, uid)
         self.nPosts = nPosts
         d = {   'g': groupInfo.id,
                 'a': userInfo.id,
