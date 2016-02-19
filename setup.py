409a99eaed338d9f7f361df69c1b2bc287133d6b
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-19 11:57:38 -0800

    Upating the product metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright © 2013 OnlineGroups.net and Contributors.
+# Copyright © 2013, 2016 OnlineGroups.net and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -12,17 +12,24 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.group.member.list'
 version = get_version()
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.rst"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
 
-setup(name='gs.group.member.list',
+setup(
+    name=name,
     version=version,
     description="The page that lists the members of the group",
-    long_description=open("README.rst").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.rst")).read(),
+    long_description=long_description,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -30,17 +37,18 @@ setup(name='gs.group.member.list',
         "Intended Audience :: Developers",
         'License :: OSI Approved :: Zope Public License',
         "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='user, group, member, group member, list',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='http://groupserver.org',
+    url='https://github.com/groupserver/{0}'.format(name),
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.member'],
+    namespace_packages=['.'.join(name.split('.')[:i])
+                        for i in range(1, len(name.split('.')))],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
@@ -53,6 +61,7 @@ setup(name='gs.group.member.list',
         'gs.content.layout',  # For the page template layout
         'gs.database',
         'gs.group.base',  # For the group marker interface
+        'gs.group.member.base',
         'gs.group.member.viewlet',
         'gs.group.privacy',
         'gs.help',
